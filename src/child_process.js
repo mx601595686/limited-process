72751efdbc56ce4b5b496cf19b52ad7024fee8c3
wujingtao <601595686@qq.com>
2017-04-16 15:05:02 +0800

    添加了新功能

--- src/child_process.js
+++ src/child_process.js
@@ -6,16 +6,82 @@ const fs = require('fs');
 const PostStream = require('post-stream');
 const vm = require('vm');
 
+const internalService = {
+    async start(requireList, jsCode, fileName){
+        global.lp.requireList = requireList.map(name => require(name));
+        process.mainModule = undefined;
+
+        vm.runInThisContext('debugger;' + jsCode, {
+            filename: fileName
+        });
+
+        await global.lp.onStart();
+    },
+    async close(){
+        await global.lp.onClose();
+    }
+};
+
 let readable = fs.createReadStream(null, {fd: 4});
 let writable = fs.createWriteStream(null, {fd: 3});
+const ps = new PostStream(readable, writable);
 
-global.ps = new PostStream(readable, writable);
-global.ps.data.once('_start', (option, code) => {
-    const requireList = Array.isArray(option.require) ? option.require : [];
-    global.requireList = requireList.map(name => require(name));
-    process.mainModule = undefined;
-    global.ps.send('_start_finish');
-    vm.runInThisContext(code, {
-        filename: option.filename
-    });
+//receive Invoke
+ps.data.on('_invoke', async function (isInternal, functionName, args, callback) {
+    const service = isInternal ? internalService : global.lp.service;
+
+    try {
+        const result = await service[functionName](...args);
+        ps.send(callback, undefined, result);
+    } catch (e) {
+        ps.send(callback, {message: e.message, stack: e.stack});
+    }
 });
+
+function sendInvoke(isInternal, functionName, ...args) {
+    const callback = '_' + Math.random();
+    ps.send('_invoke', isInternal, functionName, args, callback);
+
+    return new Promise((resolve, reject) => {
+        ps.data.once(callback, function (err, data) {
+            if (err !== undefined)
+                reject(err);
+            resolve(data)
+        });
+    });
+}
+
+setInterval(function () {
+    sendInvoke(true, 'updateCPUMemory', process.cpuUsage(), process.memoryUsage());
+}, 500);
+
+//region error handle
+
+async function errorHandler(err) {
+    let resolve = false;
+    try {
+        resolve = await global.lp.onError(err);
+    } catch (e) {
+        err = e;
+    }
+    if (resolve === false)
+        sendInvoke(true, 'uncaughtException', {message: err.message, stack: err.stack});
+}
+
+process.on('uncaughtException', errorHandler);
+process.on('unhandledRejection', errorHandler);
+
+//endregion
+
+global.lp = {
+    involve: new Proxy(sendInvoke.bind(null, false), {
+        get(target, property){
+            return target.bind(null, property);
+        }
+    }),
+    requireList: [],
+    service: {},
+    onClose: Promise.resolve(),
+    onStart: Promise.resolve(),
+    onError: () => false
+};
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 /**
- * Created by wujingtao on 2017/4/13.
+ * Created by wujingtao on 2017/4/15.
  */
 
 const child_process = require('child_process');
@@ -10,33 +10,38 @@ const PostStream = require('post-stream');
 
 module.exports = class LimitedProcess extends EventEmiter {
 
-    static runFile(filePath, option = {}) {
-        const stats = fs.statSync(filePath);
+    fileName = `tempJS:${Math.random()}`;
+    jsCode = '';
+    requireList = [];
+    service = {};
+    cpuUsage;
+    memoryUsage;
+    errors = [];
+    isRunning = false;
+    startTime;
 
-        if (stats.isFile()) {
-            const file = fs.readFileSync(filePath, 'utf8');
-
-            if (option.filename !== undefined)
-                option.filename = filePath;
-
-            LimitedProcess.runCode(file, option);
-        } else {
-            throw new Error(`path: ${filePath} is not a file`);
+    constructor(fileName, jsCode) {
+        super();
+        if ('string' === typeof jsCode) {
+            this.jsCode = jsCode;
+            if ('string' === typeof fileName) {
+                this.fileName = fileName;
+            }
+        } else if ('string' === typeof fileName) {
+            const stats = fs.statSync(fileName);
+            if (stats.isFile()) {
+                this.jsCode = fs.readFileSync(fileName, 'utf8');
+                this.fileName = fileName;
+            } else {
+                throw new Error(`path: ${fileName} is not a file`);
+            }
         }
     }
 
-    static runCode(code = '', option = {}) {
-        const startArgs = [path.resolve(__dirname, './child_process.js')];
+    start(option = {debug: false}) {
+        if (this.isRunning) return;
 
-        option = Object.assign({
-            debug: false,
-            filename: `cp:${Math.random()}`,
-            require: [],
-            uid: undefined,
-            gid: undefined,
-            cwd: undefined,
-            env: undefined
-        }, option);
+        const startArgs = [path.resolve(__dirname, './child_process.js')];
 
         if (option.debug) {
             const debug = ['--inspect'];
@@ -45,84 +50,145 @@ module.exports = class LimitedProcess extends EventEmiter {
             startArgs.unshift(debug.join('='))
         }
 
-        const child = child_process.spawn(process.execPath, startArgs, {
+        //region create child process
+
+        this._childProcess = child_process.spawn(process.execPath, startArgs, {
             stdio: ['pipe', 'pipe', 'pipe', 'pipe', 'pipe'],
             uid: option.uid,
             gid: option.gid,
             cwd: option.cwd,
             env: option.env
         });
-        const ps = new PostStream(child.stdio[3], child.stdio[4]);
-        ps.send('_start', option, code);
-        return new LimitedProcess(child, ps, option);
-    }
 
-    connected = false;
-    ps;
-    debugAddress;
-    _childProcess;
-
-    get pid() {
-        return this._childProcess.pid;
-    };
-
-    get stderr() {
-        return this._childProcess.stderr;
-    };
-
-    get stdin() {
-        return this._childProcess.stdin;
-    };
-
-    get stdout() {
-        return this._childProcess.stdout;
-    };
-
-    constructor(childProcess, ps, option) {
-        super();
-
-        this.ps = ps;
-        this._childProcess = childProcess;
-
-        if (option.debug) {
-            childProcess.stderr.once('data', data => {
-                data = data.toString();
-                const match = data.match(/(?!\s)(chrome-devtools.+)(?!\b)/g);
-                if (match.length > 0){
-                    this.debugAddress = match[0];
-                    this.emit('debugStart');
-                }
-            });
-        }
-
-        ps.data.once('_start_finish', () => {
-            this.connected = true;
-            this.emit('connected');
-        });
-
-        childProcess.on('close', (...args) => {
-            this.connected = false;
+        this._childProcess.on('close', (...args) => {
+            this._clear();
             this.emit('close', ...args);
         });
 
-        childProcess.on('error', (...args) => {
+        this._childProcess.on('error', (...args) => {
+            this._clear();
             this.emit('error', ...args);
-            this.connected = false;
         });
 
-        childProcess.on('exit', (...args) => {
+        this._childProcess.on('exit', (...args) => {
+            this._clear();
             this.emit('exit', ...args);
-            this.connected = false;
         });
+
+        this.isRunning = true;
+        this.startTime = new Date();
+        this._ps = new PostStream(this._childProcess.stdio[3], this._childProcess.stdio[4]);
+        this._ps.data.on('__invoke', this._receivedInvoke);
+
+        //endregion
+
+        if (option.debug) {
+            return new Promise((resolve, reject) => {
+                this._childProcess.stderr.once('data', data => {
+                    data = data.toString();
+                    const match = data.match(/(?!\s)(chrome-devtools.+)(?!\b)/g);
+                    if (match.length > 0) {
+                        resolve({
+                            address: match[0],
+                            start: () => {
+                                this._sendInvoke(true, 'start', this.requireList, this.jsCode, this.fileName);
+                            }
+                        })
+                    } else {
+                        reject(new Error('parse debug path failed：' + data));
+                    }
+                });
+            });
+        } else {
+            this._sendInvoke(true, 'start', this.requireList, this.jsCode, this.fileName);
+        }
     }
 
-    kill(signal) {
-        return this._childProcess.kill(signal);
-    }
+    invoke = new Proxy(this._sendInvoke.bind(null, false), {
+        get(target, property){
+            return target.bind(null, property);
+        }
+    });
 
     setTimeout(delay) {
         setTimeout(() => {
             this.kill();
         }, delay);
     }
+
+    //region internal
+
+    _childProcess;
+    _ps;
+
+    _internalService = {
+        updateCPUMemory: (cpu, memory) => {
+            this.cpuUsage = cpu;
+            this.memoryUsage = memory;
+        },
+        uncaughtException: (err) => {
+            this.errors.push(err);
+            this.emit('childProcessError', err);
+        }
+    };
+
+    _receivedInvoke = async (isInternal, functionName, args, callback) => {
+        const service = isInternal ? this._internalService : this.service;
+
+        try {
+            const result = await service[name](...args);
+            this._ps.send(callback, undefined, result);
+        } catch (e) {
+            this._ps.send(callback, {message: e.message, stack: e.stack});
+        }
+    };
+
+    _sendInvoke = (isInternal, functionName, ...args) => {
+        const callback = '_' + Math.random();
+        this._ps.send('_invoke', isInternal, functionName, args, callback);
+        return new Promise((resolve, reject) => {
+            this._ps.data.once(callback, function (err, data) {
+                if (err !== undefined)
+                    reject(err);
+                resolve(data)
+            });
+        });
+    };
+
+    _clear() {
+        this.startTime = undefined;
+        this.isRunning = false;
+        this._childProcess = undefined;
+        this._ps = undefined;
+        this.cpuUsage = undefined;
+        this.memoryUsage = undefined;
+        this.errors = [];
+    }
+
+    //endregion
+
+    //region wrap method
+
+    async kill(signal) {
+        await  this._sendInvoke(true, 'close');
+        this._childProcess && this._childProcess.kill(signal);
+    }
+
+    get pid() {
+        return this._childProcess && this._childProcess.pid;
+    };
+
+    get stderr() {
+        return this._childProcess && this._childProcess.stderr;
+    };
+
+    get stdin() {
+        return this._childProcess && this._childProcess.stdin;
+    };
+
+    get stdout() {
+        return this._childProcess && this._childProcess.stdout;
+    };
+
+    //endregion
 };
